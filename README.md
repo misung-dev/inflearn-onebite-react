d783c21bec66c22748c26fadc0fcfd9b19d550cf
misung.dev <misung.dev@gmail.com>
2025-01-16 01:52:03 +0900

    feat: Node.js 라이브러리 사용하기

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # one-bite-react
 
 [인프런] 한입 크기로 잘라 먹는 리액트(React.js) : 기초부터 실전까지
+
+## 유용 사이트
+
+- [npm.js](https://www.npmjs.com/)
--- section03/src/index.js
+++ section03/src/index.js
@@ -5,6 +5,11 @@
 // ESM (확장자를 꼭 붙여줘야함)
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(3, 2));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(3, 2));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
