0cfc744f0741b91fb6afe9449be3fd4a96987085
misung.dev <misung.dev@gmail.com>
2025-01-15 21:56:04 +0900

    feat: Node.js 모듈 시스템 이해하기

---
+++ section03/src/index.js
@@ -0,0 +1,10 @@
+// common JS
+// const moduleData = require("./math");
+// const { add, sub } = require("./math");
+
+// ESM (확장자를 꼭 붙여줘야함)
+import mul, { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(3, 2));
---
+++ section03/src/math.js
@@ -0,0 +1,29 @@
+// math 모듈
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// common JS
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+// ESM (ES Module) 방법
+// export { add, sub };
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}
