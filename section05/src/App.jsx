8578c08ce655150a0d3d2d2f357b37ef4561f802
misung.dev <misung.dev@gmail.com>
2025-01-16 23:51:56 +0900

    5.6) State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,32 @@
 import "./App.css";
-
-import Header from "./components/Header";
-import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-        <div>자식요소</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "OFF" ? "ON" : "OFF");
+          }}
+        >
+          {light === "OFF" ? "켜기" : "끄기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 }
