5b3073582d4ceb4d170692b44fdff13fa506eae4
misung.dev <misung.dev@gmail.com>
2025-01-17 01:45:34 +0900

    5.9) State로 사용자 입력 관리하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -7,48 +7,51 @@ import { useState } from "react";
 // 4. 자기소개
 
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
-
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
-      </div>
-      <div>
-        <input value={birth} type="date" onClick={onChangeBirth} />
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
 
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
+      </div>
+
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
           <option></option>
           <option>한국</option>
           <option>미국</option>
           <option>영국</option>
         </select>
-        {country}
       </div>
 
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
+        <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
     </div>
   );
