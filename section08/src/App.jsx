6ba846512401a520d77c98a85a2378d02fe99cb6
misung.dev <misung.dev@gmail.com>
2025-01-22 00:53:22 +0900

    8.2) UI 구현하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./components/Header";
+import List from "./components/List";
+import Editor from "./components/Editor";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  return <div>todolist</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
