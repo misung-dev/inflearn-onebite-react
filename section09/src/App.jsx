b17bb32182cf00b6c6dffeb8154fc2eb27e4b4a8
misung.dev <misung.dev@gmail.com>
2025-01-25 18:52:10 +0900

    9.1) useReducer를 소개합니다

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
+import Exam from "./components/Exam";
 
 const mockData = [
   {
@@ -61,9 +62,10 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Exam />
+      {/* <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
     </div>
   );
 }
