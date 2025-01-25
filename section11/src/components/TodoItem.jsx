5cab0029efec0a98d60f9a53afb8ccb22aba7404
misung.dev <misung.dev@gmail.com>
2025-01-25 23:13:39 +0900

    section11 세팅

---
+++ section11/src/components/TodoItem.jsx
@@ -0,0 +1,42 @@
+import "./TodoItem.css";
+import { memo } from "react";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckBox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckBox}
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+// 고차 컴포넌트(Higher Order Component)
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+//   // T -> Props 바뀌지 않음 -> 리렌더링
+//   // F -> Props 바뀜 -> 리렌더링O
+
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+
+//   return true;
+// });
+
+export default memo(TodoItem);
