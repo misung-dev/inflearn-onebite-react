68d7b65f8bcd44108cda33e041afe48d76cf10e9
misung.dev <misung.dev@gmail.com>
2025-01-25 22:11:38 +0900

    10.2) useMemo와 연산 최적화

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem.jsx";
 import { useState } from "react";
@@ -20,9 +21,28 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData 호출!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // 의존성 배열: deps
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
